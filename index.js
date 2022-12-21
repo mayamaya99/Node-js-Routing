75c03e0f2a661642bd890187419f94940113d949
mayamaya99 <elmi33@hotmail.com>
2022-12-20 20:28:12 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require("http");
 
 let onRequest = (request, response) => {
-    if (request.method == `GET` && request.url == "/") {
-        console.log("user made request");
-        console.log(request.url);
-        console.log(request.method);
-    }
+  // condition
+  if (request.method == `GET` && request.url == "/") {
+    console.log("user request");
+    console.log(request.url);
+    console.log(request.method);
+  }
 };
+//Create server that send back response
 
 http.createServer(onRequest).listen(3000, "127.0.0.1");
