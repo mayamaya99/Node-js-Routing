37d51028b18381f3bbb8df7f598d1b73cf4641a9
mayamaya99 <elmi33@hotmail.com>
2022-12-20 21:30:52 -0800

    Error 404

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const http = require("http");
 let serveAsEntryPoint = (response) => {
   response.writeHead(200, { "content-Type": "text/html" }); // html text
   response.write("<h3>This is header3 of this app.</h3>"); // return h3
-  response.write("<a href ='/nextpage'>Next Page  </a>");
+  response.write("<a href ='/second page'>Next Page  </a>");
   response.end();
 };
 // page two
 let serveToSecondPage = (response) => {
   response.writeHead(200, { "content-Type": "text/html" });// code 200 all good
-  response.write("<h2>This is Next page of this app.</h2>");
+  response.write("<h2>This is second page of this app.</h2>");
   response.write("<a href ='/'>Main Page  </a>");
   response.end();
 };
@@ -21,7 +21,7 @@ let send404Error = (response) => {
 let onRequest = (request, response) => {
   if (request.method == "GET" && request.url == "/") { // return url & GET
     serveAsEntryPoint(response);
-  } else if (request.method == "GET" && request.url == "/nextpage")
+  } else if (request.method == "GET" && request.url == "/secondpage")
     serveToSecondPage(response);
   else {
     send404Error(response);
